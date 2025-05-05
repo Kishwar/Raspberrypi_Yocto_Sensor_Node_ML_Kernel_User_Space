8feeeda6a2f9b46631f95a3ed55d0f148533f032
Kishwar Kumar <kumar.kishwar@gmail.com>
2025-05-05 20:36:13 +0200

    FileIO - Next operation

---
+++ UserSpace/Application/services/fileio/inc/FileIO.hpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *  @file       FileIO.hpp
+ *  @brief      Implements File Service (Read / Write operations) - accessible
+ *              via File Queue.
+ *
+ *  @copyright  Copyright (c) 2025 Kishwar Kumar
+ *              All rights reserved.
+ *
+ *  @license    This project is publicly available on GitHub for viewing and
+ *              educational/reference purposes only. Redistribution, modification,
+ *              or commercial use of any portion of this code is strictly
+ *              prohibited without prior written permission from the author.
+ *
+ *  @author     Kishwar Kumar
+ *  @version    1.0
+ *
+ *  @note       This code is not open source. Unauthorized use is not permitted.
+ ******************************************************************************/
+
+#ifndef _FILEIO_HPP_
+#define _FILEIO_HPP_
+
+#include "FileIOIf.hpp"
+
+class FileIO : protected FileIOIf {
+
+};
+
+#endif  // _FILEIO_HPP_
---
+++ UserSpace/Application/services/fileio/interface/FileIOIf.hpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *  @file       FileIOIf.hpp
+ *  @brief      Implements File Service (Read / Write operations) - accessible
+ *              via File Queue.
+ *
+ *  @copyright  Copyright (c) 2025 Kishwar Kumar
+ *              All rights reserved.
+ *
+ *  @license    This project is publicly available on GitHub for viewing and
+ *              educational/reference purposes only. Redistribution, modification,
+ *              or commercial use of any portion of this code is strictly
+ *              prohibited without prior written permission from the author.
+ *
+ *  @author     Kishwar Kumar
+ *  @version    1.0
+ *
+ *  @note       This code is not open source. Unauthorized use is not permitted.
+ ******************************************************************************/
+
+ #ifndef _FILEIO_IF_HPP_
+ #define _FILEIO_IF_HPP_
+
+class FileIOIf {
+    
+};
+
+ #endif  // _FILEIO_IF_HPP_
